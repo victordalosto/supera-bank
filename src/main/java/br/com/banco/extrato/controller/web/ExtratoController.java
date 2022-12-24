dbb2a108b6c9c3c25ebd7484210059e633d18bc7
Victor Dalosto <victordalosto@gmail.com>
2022-12-24 17:12:45 -0300

    principais funcionalidades web implementada

--- src/main/java/br/com/banco/extrato/controller/web/ExtratoController.java
+++ src/main/java/br/com/banco/extrato/controller/web/ExtratoController.java
@@ -2,11 +2,14 @@ package br.com.banco.extrato.controller.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import br.com.banco.extrato.controller.api.ExtratoRestController;
 import br.com.banco.extrato.model.TransferenciaDTO;
 import br.com.banco.extrato.service.ExtratoService;
@@ -29,11 +32,11 @@ public class ExtratoController {
 
 
     @GetMapping("/{id}")
-    public String getPaginaExtrato(@PathVariable Integer id, 
+    public String getPaginaExtrato(@PathVariable Integer id, @PageableDefault(sort = "dataTransferencia", direction = Direction.DESC, page = 0, size = 10)
                                    Pageable paginacao, Model model) {
         DadosUsuarioDTO dadosUsuario = usuarioRestController.obtemExtratoBancarioPorId(id).getBody();
         model.addAttribute("dadosUsuario", dadosUsuario);
-        Page<TransferenciaDTO> listaExtrato = extratoRestController.obtemExtratoPorId(id, null).getBody();
+        Page<TransferenciaDTO> listaExtrato = extratoRestController.obtemExtratoPorId(id, paginacao).getBody();
         model.addAttribute("listaExtrato", listaExtrato.getContent());
         return "extrato";
     }
